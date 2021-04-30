f99fea8e629b33d3385b5ae5e7b8b2481651bcb7
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-04-30 10:55:41 -0300

    Finalizado secao de hacks

---
+++ BONUS RestWithSpringBootUdemy Archetype/src/main/java/br/com/erudio/controller/PersonController.java
@@ -0,0 +1,114 @@
+package br.com.erudio.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.services.PersonServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Person Endpoint", tags = { "PersonEndpoint" })
+//@CrossOrigin
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+	@Autowired
+	private PersonServices services;
+
+	@Autowired
+	private PagedResourcesAssembler<PersonVO> assembler;
+
+	// @CrossOrigin(origins = "http://localhost:8080")
+	@ApiOperation(value = "Find all people recorded")
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
+		Page<PersonVO> list = services.findAll(pageable);
+		list.stream().forEach(personVO -> personVO
+				.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel()));
+		PagedResources<?> resources = assembler.toResource(list);
+		return new ResponseEntity<>(resources, HttpStatus.OK);
+	}
+
+	// @CrossOrigin(origins = "http://localhost:8080")
+	@ApiOperation(value = "Find person by name")
+	@GetMapping(value = "/findPersonByName/{firstName}", produces = { "application/json", "application/xml",
+			"application/x-yaml" })
+	public ResponseEntity<?> findPersonByName(
+			@PathVariable(value = "firstName") String firstName,
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
+		Page<PersonVO> list = services.findPersonByName(firstName, pageable);
+		list.stream().forEach(personVO -> personVO
+				.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel()));
+		PagedResources<?> resources = assembler.toResource(list);
+		return new ResponseEntity<>(resources, HttpStatus.OK);
+	}
+
+	// @CrossOrigin(origins = {"http://localhost:8080", "http://www.erudio.com.br"})
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		PersonVO personVO = services.findById(id);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return personVO;
+	}
+
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public PersonVO create(@RequestBody PersonVO person) {
+		PersonVO personVO = services.create(person);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public PersonVO update(@RequestBody PersonVO person) {
+		PersonVO personVO = services.update(person);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		services.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
+	@ApiOperation(value = "Disable a specific person by ID")
+	@PatchMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO disablePerson(@PathVariable(value = "id") Long id) {
+		PersonVO personVO = services.disablePerson(id);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return personVO;
+	}
+}
---
+++ BONUS RestWithSpringBootUdemy Archetype/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -0,0 +1,63 @@
+package br.com.erudio.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.exception.FileStorageException;
+import br.com.erudio.exception.MyFileNotFoundException;
+
+@Service
+public class FileStorageService {
+
+	private final Path fileStorageLocation;
+
+	@Autowired
+	public FileStorageService(FileStorageConfig fileStorageConfig) {
+		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+		try {
+			Files.createDirectories(this.fileStorageLocation);
+		} catch (Exception ex) {
+			throw new FileStorageException("Could not create the directory where the uploaded files will be stored",
+					ex);
+		}
+	}
+
+	public String storeFile(MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		try {
+			if (fileName.contains("..")) {
+				throw new FileStorageException("Filename contains invalid path sequence " + fileName);
+			}
+			//Aqui define que armazena em disco
+			Path targetLocation = this.fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			return fileName;
+		} catch (Exception e) {
+			throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String filename) {
+		try {
+			//
+			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if(resource.exists()) {
+				return resource;
+			}
+			throw new MyFileNotFoundException("File not found " + filename);
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + filename, e);
+		}
+	}
+}
